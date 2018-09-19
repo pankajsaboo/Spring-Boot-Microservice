753cba83f533da4e987f1f4f1e539123d763f855
synerzip <pankaj.saboo08@gmail.com>
2018-09-19 18:48:31 +0530

    discovery service

--- customer/src/main/java/com/customer/config/CustomerApplication.java
+++ customer/src/main/java/com/customer/config/CustomerApplication.java
@@ -3,6 +3,7 @@ package com.customer.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.customer.repository")
 @ComponentScan(basePackages = "com.customer.*")
 @EntityScan("com.customer.*")
+@EnableDiscoveryClient
 public class CustomerApplication {
 
 	public static void main(String[] args) {
--- orderservice/src/main/java/com/orderservice/config/OrderserviceApplication.java
+++ orderservice/src/main/java/com/orderservice/config/OrderserviceApplication.java
@@ -2,9 +2,11 @@ package com.orderservice.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan("com.orderservice.controller")
 public class OrderserviceApplication {
 
